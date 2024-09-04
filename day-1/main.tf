97d8578e3767da649ab263aaa8d950e573a0ced6
Vivek Vishal <vivekvishal392@gmail.com>
2024-09-04 19:54:45 +0530

    all done

--- day-1/main.tf
+++ day-1/main.tf
@@ -2,10 +2,9 @@ resource "aws_instance" "dev" {
   ami = var.ami_id
   instance_type = var.instance-type
   key_name = var.keyname
-  count = 2
     tags = {
-        Name = "my-instance-${count.index}"
-    }           
+        Name = "my-instance"
+        }           
 
 }
 
--- day-1/terraform.tfvars
+++ day-1/terraform.tfvars
@@ -1,3 +1,3 @@
 ami_id = "ami-0ae8f15ae66fe8cda"
-instance-type = "t2.micro"
+instance-type = "t2.medium"
 keyname = "cfn-key-1"
